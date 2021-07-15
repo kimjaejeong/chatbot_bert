96fe69c103f4374ca3d47dcfb25f1b2013814cd4
김재정 <jaejeong.kim@kbfg.com>
2021-07-15 13:56:40 +0900

    문구 수정 완료

--- 04_s3_connection_telegram.py
+++ 04_s3_connection_telegram.py
@@ -8,15 +8,14 @@ from datetime import datetime
 import pytz
 import sqlite3
 
-
-############# S3 연동 키 ############################
-AWS_ACCESS_KEY_ID = 'AKIATY67EXXXXXXXXXXX'
-AWS_ACCESS_KEY_SECRET = 'qzj1JFCdXVuZDER3XXXXXXXXXXX'
-region_name = 'ap-noXXXXXXXXXXX'
-
-############# 텔레그램 연동 키 ############################
-api_key = "1830582407:AAElfZrgBT637jcUxxxxxxxx"
-chat_id = "1772xxxxx"
+# ############# S3 연동 키 ############################
+# AWS_ACCESS_KEY_ID = 'AKIATY67EXXXXXXXXXXX'
+# AWS_ACCESS_KEY_SECRET = 'qzj1JFCdXVuZDER3XXXXXXXXXXX'
+# region_name = 'ap-noXXXXXXXXXXX'
+#
+# ############# 텔레그램 연동 키 ############################
+# api_key = "1830582407:AAElfZrgBT637jcUxxxxxxxx"
+# chat_id = "1772xxxxx"
 
 # S3 버킷에 있는 데이터 불러오기
 def load_data_s3():
@@ -34,6 +33,9 @@ def load_data_s3():
 
 # news_qa API 호출
 def news_qa_analysis(question, original_news_data):
+    # from konlpy.tag import Okt  # 예시: 광주광역시가 7월 1일에 시행하는 것은?
+    #
+    # okt = Okt()
 
     url = "http://172.16.50.4:9000/qa_analysis"
 
@@ -41,6 +43,8 @@ def news_qa_analysis(question, original_news_data):
                'original_news_data': original_news_data,
                }
     answer_sentence = requests.request("POST", url, data=payload)
+
+    # after_tuning_answer_words = ' '.join(okt.phrases(answer_sentence.text))
 
     return answer_sentence.text
 
@@ -85,14 +89,16 @@ def handler(update, context):
     
     # 경제 내용 요약
     if (user_text in ["경제", "IT/과학", "정치", "사회", "생활/문화", "스포츠"]):
-        bot.send_message(chat_id=chat_id, text= user_text + " 관련 따끈따끈한 최신 뉴스 전달 드립니다^^")
+        bot.send_message(chat_id=chat_id, text= user_text + " 관련 최신 뉴스 전달 드립니다\n" + "(AI가 {} 기사로 분류했습니다.)".format(user_text))
         # 해당 뉴스 불러오기
         grid_sizes = load_data_s3()
         dropna_grid_sizes = grid_sizes[grid_sizes.CONTENT.notnull()]
         top1_df = dropna_grid_sizes[dropna_grid_sizes.CATEGORY == user_text].sort_values("WRITEDATE", ascending=False).head(1)
-        AI_message = "AI 분석 요약 : \n" + top1_df.SUMMARY_CONTENT.iloc[0]
-        bot.send_message(chat_id=chat_id, text=top1_df.TITLE.iloc[0] + '\n\n' + AI_message + '\n\n' + top1_df.URL.iloc[0])
-        bot.send_message(chat_id=chat_id, text="해당 뉴스에서 궁금한 사항이 있으면 입력해주세요." + "없으면 '종료'를 입력해주세요")
+        title_message = "[뉴스 제목] \n" + top1_df.TITLE.iloc[0]
+        url_message = top1_df.URL.iloc[0]
+        AI_message = "[AI 분석 요약] \n" + " " + top1_df.SUMMARY_CONTENT.iloc[0]
+        bot.send_message(chat_id=chat_id, text=title_message + '\n\n' + AI_message + '\n\n' + url_message)
+        bot.send_message(chat_id=chat_id, text="해당 뉴스에서 궁금한 사항이 있으면 입력해주세요." + "\n없으면 '종료'를 입력해주세요")
 
         # 해당 관련 DB 저장 - original_news_data / date
         insert_db(top1_df.CONTENT.iloc[0])
@@ -100,18 +106,19 @@ def handler(update, context):
     elif (user_text == "종료"):
         # athena DB 내용 삭제
         delete_db()
-        bot.send_message(chat_id=chat_id, text="다른 카테고리가 궁금하시면 입력해주세요.\n" + '''"경제", "IT/과학", "정치", "사회", "생활/문화", "스포츠"''')
+        bot.send_message(chat_id=chat_id, text="다른 분야가 궁금하시면 입력해주세요.\n\n" + '''"경제", "IT/과학", "정치", \n "사회", "생활/문화", "스포츠"''')
     
     else: # 질문 관련 내용
         # DB에서 내용 최신 순으로 조회 후, news_qa analysis에 호출
         #// 아무것도 없을 때 exceptio 처리 해야 함.
         original_news_data = select_db()
-        answer_sentence = news_qa_analysis(user_text, original_news_data)  # 예시: 광주광역시가 7월 1일에 시행하는 것은?
-        bot.send_message(chat_id=chat_id, text=answer_sentence)
+        answer_message = news_qa_analysis(user_text, original_news_data)
+
+        bot.send_message(chat_id=chat_id, text=answer_message)
 
 if __name__ == "__main__":
     bot = telegram.Bot(api_key)
-    info_message = '''- 원하는 뉴스 카테고리 입력 : '"경제", "IT/과학", "정치", "사회", "생활/문화", "스포츠"'''
+    info_message = '''[원하는 뉴스 분야 입력] \n\n '"경제", "IT/과학", "정치", \n"사회", "생활/문화", "스포츠"'''
     bot.sendMessage(chat_id=chat_id, text=info_message)
 
     updater = Updater(token=api_key, use_context=True)
